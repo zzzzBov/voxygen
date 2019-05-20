da22162256c4c625984a97d6e79d3cb17d93bf8f
Tim Leverett <zzzzbov@gmail.com>
2019-05-20 12:06:52 -0400

    Updates

---
+++ precache-manifest.5882267642cbaed8d0f6edcd2025029c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a629df506eb64bbe09b3f150568c32d",
+    "url": "/voxygen/index.html"
+  },
+  {
+    "revision": "11ce7310150c9586d8a6",
+    "url": "/voxygen/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/voxygen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "11ce7310150c9586d8a6",
+    "url": "/voxygen/static/js/main.bb0944b2.chunk.js"
+  },
+  {
+    "revision": "468e7dc01ab2989d5016",
+    "url": "/voxygen/static/js/runtime~main.0d95842e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/voxygen/static/media/logo.5d5d9eef.svg"
+  }
+]);
